7cf7f87b6ed1b9a27145f61acd354ce5ff2864ba
pooja <poojaanbuchezhian@gmail.com>
2020-11-03 17:25:35 +0530

    added components

--- video_library_frontend/src/components/ListVideos.vue
+++ video_library_frontend/src/components/ListVideos.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="">
+    <div class="col-md-12 mb-5" v-if="createNew">
+      <CreateVideo v-on:createdVideo="updateVideos"/>
+    </div>
     <div class="row">
       <div class="col-md-5 text-center nicefont">
         <h4>Welcome to Youtube Rater</h4>
+        <form @submit="createdNew()">
+            <input class="btn-sm btn-primary mb-3 btn-center" id="cretedNew" type="submit" value="Create new video">
+
+          </form>
           <p v-bind:key="video.id" v-for="video in videos">
                {{video.title}}
                <br>
                Rating: {{video.rating_average}}
                <br>
-            <button class="btn-sm btn-primary mt-2 mb-3">Details</button>
+            <button class="btn-sm btn-primary mt-2 mb-3" v-on:click="videoDetail(video)">Details</button>
           </p>
       </div>
+      <div class="col-md-6">
+        <VideoDetails  v-bind:videodetail="videodetail"/>
+      </div>
+
     </div>
 
   </div>
@@ -19,15 +30,20 @@
 <script>
 // @ is an alias to /src
 import axios from 'axios';
+import VideoDetails from './VideoDetails';
+import CreateVideo from './CreateVideo';
 
 export default {
   name: 'Home',
   components: {
-
+    VideoDetails,
+    CreateVideo,
   },
   data() {
     return {
       videos : [],
+      videodetail: Object,
+      createNew: "",
     }
   },
   methods: {
@@ -35,13 +51,21 @@ export default {
       axios.get("http://127.0.0.1:8000/api/videos/")
       .then(res => (this.videos = res.data))
       .catch(err => console.log(err));
-    }
+  },
+  videoDetail(video) {
+     this.videodetail = video;
+     console.log(this.videodetail)
+   },
+   createdNew() {
+      this.createNew = !this.createNew;
+    },
   },
   created() {
     this.getVideos();
     createNew: false;
   }
-}
+};
+
 </script>
 
 <style scoped>
---
+++ video_library_frontend/src/components/VideoDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <div class="row">
+      <div class="col-md-10 "  >
+        <p class="mb-3">Title: {{videodetail.title}}</p>
+        <p>Description: {{videodetail.description}}</p>
+        <p>Rating: {{videodetail.rating_average}}</p>
+        <p>Comments: {{videodetail.comments_list}}</p>
+        <p>Category: {{videodetail.category}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+
+export default {
+  name: 'VideoDetails',
+  components: {
+
+  },
+  props: {
+    videodetail: {}
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
